d84a80f84d05bc3ecf46ee52866c20d3cfc611f6
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-05-31 16:49:19 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Test
+{
+public static void main(String args[])
+{
+int a= 10,b=20;
+System.out.println("value of a="+a);
+System.out.println(a+b);
+System.out.println(a+"Hello"+b);
+System.out.println(a+b+"Hello");
+System.out.println("Hello"+a+b);
+System.out.println("Hello"+(a+b));
+}
+}
---
+++ TestByte.java
@@ -0,0 +1,12 @@
+class TestByte
+{
+public static void main(String args[]) 
+{
+byte a=10,b=20,c;
+a=(byte)(a+5);
+c=(byte)(a+b);
+
+System.out.println(a);
+System.out.println(c);
+}
+}
