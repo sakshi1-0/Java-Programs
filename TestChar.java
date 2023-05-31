bd36cea0a853560eb843670e8fea96fae866ffab
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-05-31 16:50:19 +0530

    Add files via upload

---
+++ TestChar.java
@@ -0,0 +1,12 @@
+class TestChar
+{
+public static void main(String args[]) 
+{
+char ch1='A';
+char ch2=65;
+char ch3='\u0041';
+System.out.println(ch1);
+System.out.println(ch2);
+System.out.println(ch3);
+}
+}
---
+++ TestFloat.java
@@ -0,0 +1,10 @@
+class TestFloat
+{
+public static void main(String args[]) 
+{
+float a=10.65f;
+double c=10.65f;
+System.out.println(a);
+System.out.println(c);
+}
+}
