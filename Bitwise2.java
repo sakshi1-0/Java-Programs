1709e7daccf557cdcae9f78a2119f2bee1a6ad4e
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-02 15:01:15 +0530

    Add files via upload

---
+++ Bitwise2.java
@@ -0,0 +1,11 @@
+class Bitwise2{
+public static void main(String args[])
+{
+int a=15 & 7;
+int b=10 | 7;
+int c=10^7;
+int d=-19<<3;
+int e=12>>2;
+System.out.println(a+" "+b+" "+c+" "+d+" "+e);
+}
+}
