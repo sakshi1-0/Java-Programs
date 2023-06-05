259fdeff9838125404bbf64bc1052d6b90533256
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-05 16:48:40 +0530

    Add files via upload

---
+++ SwitchNum.java
@@ -0,0 +1,29 @@
+class SwitchNum{
+public static void main(String args[])
+{
+int a=3;
+switch(a){
+case 1:System.out.println("one");
+break;
+
+case 2:
+System.out.println("two");
+break;
+
+case 3:
+System.out.println("three");
+break;
+
+case 4:
+System.out.println("four");
+break;
+
+case 5:
+System.out.println("five");
+break;
+
+default:
+System.out.println("Invalid number");
+}
+}
+}
