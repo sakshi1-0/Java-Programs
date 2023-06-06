b75ef7a032de3107443791db229acfcd99d198ee
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-06 14:32:58 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+class Factorial{
+public static void main(String args[])
+{
+int a=0,i=1,f=1;
+if(a==0){
+System.out.println(a);
+}
+else{
+while(i<=a){
+
+f=f*i;
+i++;
+}
+
+System.out.println("Factorial="+f);
+}
+
+}
+}
