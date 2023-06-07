9443f25eac99acec2342532ca2879bbf4234d8b1
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-07 14:37:12 +0530

    Add files via upload

---
+++ LogicalOr.java
@@ -0,0 +1,11 @@
+class LogicalOr{
+public static void main(String args[])
+{
+int a=10,b=10,c=10;
+boolean x;
+x=a++==b++ || --b==++c;            //short circuit
+System.out.println(x);
+System.out.println(a+" "+b+" "+c);
+
+}
+}
