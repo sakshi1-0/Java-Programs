4d10e6818bc99fe7f6bbcf564b12d96bdd4cfecd
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-09 12:23:30 +0530

    Add files via upload

---
+++ While1.java
@@ -0,0 +1,13 @@
+//WAP to print your name on console for 10 times
+
+
+class While1{
+public static void main(String args[])
+{
+int i=1;
+while(i++<=10){
+System.out.println("Sakshi");
+//i++;
+}
+}
+}
