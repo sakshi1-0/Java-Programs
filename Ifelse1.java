c00ccb99e806f304046babe37f5d9dce369f370a
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-09 12:21:51 +0530

    Add files via upload

---
+++ Ifelse1.java
@@ -0,0 +1,18 @@
+class Ifelse1{
+public static void main(String args[])
+{
+int a=0;
+if(a>0)
+{
+System.out.println("Positive");
+}
+else if(a<0)
+{
+System.out.println("Nagative");
+}
+else
+{
+System.out.println("Neutral");
+}
+}
+}
