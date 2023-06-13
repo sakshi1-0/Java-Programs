1b84dc1903d25c961dd1f036f4ecc05f9e22ef61
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-13 17:02:50 +0530

    Add files via upload

---
+++ While2.java
@@ -0,0 +1,12 @@
+//WAP to print to print following series-1 2 3 4 5 6 7 8 9 10
+
+class While2{
+public static void main(String args[])
+{
+int i=1;
+while(i<=10){
+System.out.print(i+"\t");
+i++;
+}
+}
+}
---
+++ While3.java
@@ -0,0 +1,12 @@
+//WAP to print to print following series-2 4 6 8 10 12 14 16 18 20
+
+class While3{
+public static void main(String args[])
+{
+int i=1;
+while(i<=10){
+System.out.print(i*2+"\t");
+i++;
+}
+}
+}
