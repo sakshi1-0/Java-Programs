5442827302c0aac117952f9ec1c5746e6578fca2
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-16 16:53:23 +0530

    Add files via upload

---
+++ While4.java
@@ -0,0 +1,12 @@
+//WAP to print to print following series-1 4 9 16 25 36 49 64 81 100
+
+class While4{
+public static void main(String args[])
+{
+int i=1;
+while(i<=10){
+System.out.print((i*i)+"\t");
+i++;
+}
+}
+}
---
+++ While5.java
@@ -0,0 +1,14 @@
+//WAP to print to print following series-1 4 9 16 25 36 49 64 81 100 without *sign
+
+class While5{
+public static void main(String args[])
+{
+int i=1,j=3;
+while(i<=100){
+System.out.print(i+"\t");
+i=i+j;
+j=j+2;
+}
+}
+}
+
