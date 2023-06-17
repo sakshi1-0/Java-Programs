79d05f3853a1d9916cac657e64fb048ad6adfc65
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-06-17 17:42:31 +0530

    Add files via upload

---
+++ While7.java
@@ -0,0 +1,13 @@
+//WAP to print to print following series-1/1 2/4 3/9 4/16 5/25 6/36 7/49 8/64 9/81 10/100
+
+class While7
+{
+public static void main(String args[])
+{
+int i=1;
+while(i<=10){
+System.out.print(i+"/"+i*i+"\t");
+i++;
+}
+}
+}
