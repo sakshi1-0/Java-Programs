ec878ddea8d81c424d585dc87f5bca53d864acba
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-07-11 18:49:40 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,16 @@
+class Break
+{
+public static void main(String args[])
+{
+int i=1;
+for(; ;)
+{
+if(i>8)
+{
+break;
+}
+System.out.print(i+"\t");
+i++;
+}
+}
+}
---
+++ Continue.java
@@ -0,0 +1,15 @@
+class Continue
+{
+public static void main(String args[])
+{
+int i;
+for(i=1;i<=20;i++)
+{
+if(i%2==0)
+{
+continue;
+}
+System.out.print(i+" \t");
+}
+}
+}
