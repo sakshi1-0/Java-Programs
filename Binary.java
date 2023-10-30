ab9042a86dd3a9516a49eef2ac9f2ea23acf97db
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-10-30 15:01:56 +0530

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,47 @@
+//Linear Search program
+
+class Binary
+{
+public static void main(String args[])
+{
+ int a[]={40,80,60,20,40,10,50,30},temp,i,j;
+ int low=0,high=a.length-1,mid,n=490;
+ boolean f=false;
+ for(i=0;i<a.length-1;i++)
+{
+ for(j=i+1;j<a.length;j++)
+{
+ if(a[i]>a[j])
+{
+ temp=a[i];
+ a[i]=a[j];
+ a[j]=temp;
+}
+}
+}
+while(low<=high)
+{
+ mid=(low+high)/2;
+ if(a[mid]==n)
+{
+ f=true;
+ break;
+}
+ else if(n>a[mid])
+{
+ low=mid+1;
+}
+ else
+{
+ high=mid-1;
+}
+}
+ if(f)
+{
+System.out.println("Element found");
+}
+ else
+{
+System.out.println("Element not found");
+}
+}}
