1e2e0837ca7674ca3467700922408ce0ddaab7ab
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-02 21:09:53 +0530

    Add files via upload

---
+++ Constructor.java
@@ -0,0 +1,13 @@
+class Constructor
+{
+Constructor()
+{
+System.out.println("Hello");
+}
+public static void main(String args[])
+{
+Constructor c1=new Constructor();
+Constructor c2=new Constructor();
+}
+
+}
