56b3c562701de0fbe133cdb96ba466b20ccfcf5a
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-03 18:44:25 +0530

    Add files via upload

---
+++ CopyCons.java
@@ -0,0 +1,21 @@
+class ArayArgs
+{
+void add (int a[])
+{
+int sum=0;
+for(int i=0;i<a.length;i++)
+{
+sum=sum+a[i];
+}
+System.out.println(sum);
+
+}
+public static void main(String args[])
+{
+ArayArgs a=new ArayArgs();
+int x[]={10,20,30,40,50};
+int y[]={1,2,3,4,5,6,7,8,9}
+a.add(x);
+a.add(y);
+}
+}
