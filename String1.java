c11479da81dde52c73efd984ded4e605385712d0
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-07 20:13:43 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,10 @@
+class String1
+{
+public static void main(String args[])
+{
+String str=new String("Hello");
+System.out.println(str);
+System.out.println(str.hashCode());
+System.out.println(str.length());
+}
+}
