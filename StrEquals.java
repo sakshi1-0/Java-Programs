5107866efe4af5dfd8aeada62a0db6c00418a333
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-13 15:30:51 +0530

    Add files via upload

---
+++ StrEquals.java
@@ -0,0 +1,19 @@
+//Method of String class
+
+class StrEquals
+{
+public static void main(String args[])
+{
+String s1=new String("Hello");
+String s2=new String("Hello");
+
+if(s1.equals(s2))                                            
+{
+System.out.println("Same");
+}
+else
+{
+System.out.println("Different");
+}
+}
+}
