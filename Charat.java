85bfdfd2931dcd2e0ab355f9755a1933c0c07b4e
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-14 21:00:18 +0530

    Add files via upload

---
+++ Charat.java
@@ -0,0 +1,12 @@
+class Charat
+{
+public static void main(String args[])
+{
+String s1=new String ("Hello");
+System.out.println(s1.charAt(0));
+for(int i=0;i<s1.length();i++)
+{
+System.out.println(s1.charAt(i));
+}
+}
+}
