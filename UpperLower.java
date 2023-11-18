99c358c0bb05d1e9de027ee5c45dbb760c804cc5
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-18 19:48:13 +0530

    Add files via upload

---
+++ UpperLower.java
@@ -0,0 +1,12 @@
+
+
+class UpperLower
+{
+public static void main(String args[])
+{
+String str="Hello";
+
+System.out.println(str);
+System.out.println(str.toUpperCase());
+System.out.println(str.toLowerCase());
+}}
