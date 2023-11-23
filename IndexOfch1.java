d596dd1f5f5a60527a0d3599dc982384d996c2bd
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-23 16:24:45 +0530

    Add files via upload

---
+++ IndexOfch1.java
@@ -0,0 +1,9 @@
+class IndexOfch1
+{
+public static void main(String args[])
+{
+String str= new String("india");
+System.out.println(str.indexOf('i'));
+System.out.println(str.indexOf('i',1));
+}
+}
