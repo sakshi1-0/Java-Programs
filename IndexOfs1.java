623160e5550ee4bf03dec422511a0f94430c399c
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-24 14:52:33 +0530

    Add files via upload

---
+++ IndexOfs1.java
@@ -0,0 +1,9 @@
+class IndexOfs1
+{
+public static void main(String args[])
+{
+String str= new String("indore is in india");
+System.out.println(str.indexOf("in"));
+System.out.println(str.indexOf("in",1));
+}
+}
