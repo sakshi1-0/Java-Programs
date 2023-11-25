c8107f32f7848aba0e35cc324b7a7bd563ae508b
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-25 18:24:13 +0530

    Add files via upload

---
+++ StringAry.java
@@ -0,0 +1,15 @@
+//WAP to print all the elements of given String array
+
+
+class StringAry
+{
+public static void main(String args[])
+{
+String str[]={"indore","bhopal","dewas","ujjain","dhar","india","indonesia","bhind"};
+for(String s :str)
+{
+System.out.println(s);
+}
+
+}
+}
