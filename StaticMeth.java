2ccfad220a8640fb4db9528dbc1a936d5f8c4569
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2023-11-29 21:06:01 +0530

    Add files via upload

---
+++ StaticMeth.java
@@ -0,0 +1,17 @@
+//Static variable,method
+
+class StaticMeth
+{
+ static int a=70;
+ static void show()
+{
+ System.out.println(a+5); 
+}
+
+
+public static void main(String args[])
+{
+  StaticMeth.show(); 
+
+}
+} 
