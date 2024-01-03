c33d1ec6bbfeacbfdc82a3386a6a9d28cc938f2d
Sakshi Gayakwad <99253884+sakshi1-0@users.noreply.github.com>
2024-01-03 14:52:49 +0530

    Add files via upload

---
+++ FinalKeyword1.java
@@ -0,0 +1,17 @@
+//final keyword
+
+class FinalKeyword1
+{
+final int a=10;
+void show()
+{
+a++;
+System.out.println(a);
+}
+
+public static void main(String args[])
+{
+FinalKeyword1 f1 = new FinalKeyword1();
+f1.show();
+}
+}
